eaec68c02bda104df079232d88985298398a66f7
Asir Intisar <aintisar48@gmail.com>
2025-02-07 18:37:14 +0600

    [Week] - [Day-81] - [Problem-82]

--- A_Oath_of_the_Night_s_Watch.cpp
+++ A_Oath_of_the_Night_s_Watch.cpp
@@ -6,6 +6,19 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int cnt = 0;
+    sort(a.begin(),a.end());
+    for(int i=1;i<n-1;i++){
+        if(a[i] > a[0] && a[i] < a[n-1]){
+            cnt++;
+        }
+    }
+    cout<<cnt;
     return 0;
 }
