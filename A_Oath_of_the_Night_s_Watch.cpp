6f1674c6396d9de06880158a434e2efb9c536dcd
Asir Intisar <aintisar48@gmail.com>
2025-02-07 18:15:04 +0600

    [Week] - [Day-81] - [Problem-81]

---
+++ A_Oath_of_the_Night_s_Watch.cpp
@@ -0,0 +1,11 @@
+// Week 6 Day 38 Problem 82
+
+// A. Oath of the Night's Watch => https://codeforces.com/contest/768/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
